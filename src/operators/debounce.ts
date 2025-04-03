0b23b91eed53149fb1071d3f63cfa157be46d4c6
Michael De Abreu <10507776+michaeljota@users.noreply.github.com>
2025-04-03 14:01:30 +0200

    refactor: simplify debounce code

--- src/operators/debounce.ts
+++ src/operators/debounce.ts
@@ -4,16 +4,16 @@ import type { OperatorFn } from "./types";
 export function debounce<T>(delay: number): OperatorFn<T> {
   return (source) => {
     const debouncedEvent = event<T>();
-    let timeoutId: ReturnType<typeof setTimeout> | null = null;
+    let timeoutId: ReturnType<typeof setTimeout> | undefined;
 
     source.subscribe((value) => {
-      if (timeoutId !== null) {
+      if (timeoutId != null) {
         clearTimeout(timeoutId);
       }
 
       timeoutId = setTimeout(() => {
         debouncedEvent(value);
-        timeoutId = null;
+        timeoutId = void 0;
       }, delay);
     });
 
